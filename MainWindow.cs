7aac6ad97e154ddaaa034af5a95d15e3b83866f7
Lhiw <60870890+Lhiw@users.noreply.github.com>
2020-06-06 11:22:13 +0700

    lecture 60
    homework

---
+++ MainWindow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloworldGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string calculate1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }     
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double calculate1 = double.Parse(txtPrice.Text) / (double.Parse(txtIncome.Text) - double.Parse(txtExpense.Text));
+            txtDay.Text = $"{(calculate1)}";
+        }
+        private void txtDay_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
